7f8ca20b32fe77e4c47b41800c9ecbaa3b1614cd
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2021-01-06 20:00:36 -0500

    random roles

--- lol-randomizer/src/Components/Randomizer.js
+++ lol-randomizer/src/Components/Randomizer.js
@@ -15,33 +15,50 @@ const ppl =
     { id: '8', name: 'Muhammed', wins: 0, loses: 0, role: 'jungler' },
     { id: '9', name: 'Zach', wins: 0, loses: 0, role: 'jungler' },
     { id: '10', name: 'Kyle', wins: 0, loses: 0, role: 'jungler' }]
-
 const Randomizer = () => {
     const [allPlayers, setAllPlayers] = useState(ppl);
     const [blueTeam, setBlueTeam] = useState([]);
     const [redTeam, setRedTeam] = useState([]);
     const [players, setPlayers] = useState({ items: ppl, selected: [] });
+    
     const handleRandomize = () => {
-        if (players.selected.length < 4 || players.selected.length % 2 !== 0) {
+        const len = players.selected.length;
+        if (len < 6 || len % 2 !== 0) {
             return;
         }
+        const blueRoles = ["jungle", "lane", "lane"];
+        const redRoles = ["jungle", "lane", "lane"];
+        let remaining = len % 6;
+        while (remaining >= 1) {
+            blueRoles.push("lane");
+            redRoles.push("lane");
+            remaining--;
+        }
         const playerClone = Array.from(players.selected);
-        const teamA = [];
-        const teamB = [];
+        const rTeam = [];
+        const bTeam = [];
         while (playerClone.length > 0) {
             const [person] = playerClone.splice(Math.floor(Math.random() * playerClone.length), 1);
-            (playerClone.length % 2 === 0) ? teamA.push(person)
-                : teamB.push(person);
+            if (playerClone.length % 2 === 0) {
+                const [role] = redRoles.splice(Math.floor(Math.random() * redRoles.length), 1);
+                person.role=role;
+                rTeam.push(person);
+
+            } else {
+                const [role] = blueRoles.splice(Math.floor(Math.random() * blueRoles.length), 1);
+                person.role=role;
+                bTeam.push(person);
+            }
         }
-        setBlueTeam(teamA);
-        setRedTeam(teamB);
+        setBlueTeam(rTeam);
+        setRedTeam(bTeam);
     }
     return (
         <Container>
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'start', marginBottom: '5rem' }}>
                 <DnD players={players} setPlayers={setPlayers} handleRandomize={handleRandomize} />
                 {redTeam.length > 0 && blueTeam.length > 0 && (<div style={{ marginRight: '5rem', marginTop: '6rem' }}>
-                    <Teams redTeam={redTeam} blueTeam={blueTeam} players={players} setAllPlayers={setAllPlayers}  handleRandomize={handleRandomize} />
+                    <Teams redTeam={redTeam} blueTeam={blueTeam} players={players} setAllPlayers={setAllPlayers} handleRandomize={handleRandomize} />
                 </div>)}
             </div>
             <PlayerTable allPlayers={allPlayers} />
