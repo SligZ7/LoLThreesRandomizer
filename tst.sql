f2fa69b303f5dbbd621d7d60d0a7375d023bf2e3
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2021-01-07 15:04:34 -0500

    Create tst.sql

---
+++ tst.sql
@@ -0,0 +1,25 @@
+mysql> CREATE TABLE games(
+id INT NOT NULL AUTO_INCREMENT,
+game_size INT NOT NULL,
+winning_side VARCHAR(10) NOT NULL,
+winners VARCHAR(250) NOT NULL,
+losers VARCHAR(250) NOT NULL,
+PRIMARY KEY ( id ),
+);
+
+CREATE TABLE games(
+id INT NOT NULL AUTO_INCREMENT,
+game_size INT NOT NULL,
+winning_side VARCHAR(10) NOT NULL,
+winners VARCHAR(250) NOT NULL,
+losers VARCHAR(250) NOT NULL,
+PRIMARY KEY ( id )
+);
+
+INSERT INTO games (id, game_size, winning_side, winners, losers) VALUES (1, 6, "red", "Sachin-1,Parth-3,Noel-2", "Jonah-5,Mark-4,Muhammed-7");
+INSERT INTO games (id, game_size, winning_side, winners, losers) VALUES (2, 6, "red", "Sachin-1,Parth-3,Zack-10", "Jonah-5,Mark-4,Muhammed-7");
+
+select * from games where winners like "%Noel-2%"
+select * from games where winners like "%Sachin-1%"
+
+select * from (select * from games where winners like "%Sachin-1%") as sub where winners like "%Noel-2%";
