959562166a7c51d944ea41aa9798f51f294f144b
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2021-03-07 23:29:38 -0500

    Update index.jsx

--- lol-randomizer/src/Components/MatchHistory/index.jsx
+++ lol-randomizer/src/Components/MatchHistory/index.jsx
@@ -38,7 +38,7 @@ function MatchHistory(props) {
                 const red = game.red.split(",");
                 const blue = game.blue.split(',');
                 return (
-                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '15px', backgroundImage: `url(${cardBackGround})`, borderStyle: "solid", backgroundPosition: '-500px 0px', borderRadius: '5px', boxShadow: '4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', backgroundSize: '1500px' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '15px', backgroundImage: `url(${cardBackGround})`, borderStyle: "solid", backgroundPosition: '-500px 0px',  boxShadow: '4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', backgroundSize: '1500px' }}>
                         <div style={{ display: 'flex', justifyContent: 'space-between', borderBottom: '3px solid white', padding: '5px', fontWeight: 'bold',  backgroundImage: `url(${darkBg})`,  backgroundPosition: '-100px 0px' }}>
                             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'space-between', }}>
                                 {moment(game.date).tz("America/New_York").format('MMMM Do, YYYY')}
@@ -54,7 +54,7 @@ function MatchHistory(props) {
                             <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', marginRight: '20px' }}  >
                                 {blue.map((b, index) => {
                                     const [role, name] = b.split('-');
-                                    return (<div style={{ backgroundImage: game.winning_side === 'blue' ? `url(${victoryBlueIcon})` : '', backgroundPosition: `-40px -${((index + 1) * 75) - 75}px`, marginBottom: '5px', backgroundRepeat: 'no-repeat', backgroundSize: '400px' }}>
+                                    return (<div style={{ backgroundImage: game.winning_side === 'blue' ? `url(${victoryBlueIcon})` : '', backgroundPosition: `-40px -${((index + 1) * 75) - 75}px`, marginBottom: '5px', backgroundRepeat: 'no-repeat', backgroundSize: '400px',borderRadius: '5px', }}>
                                         <Player name={name} role={role} color="blue" winner={game.winning_side === "blue"} />
                                     </div>)
                                 })}
@@ -62,7 +62,7 @@ function MatchHistory(props) {
                             <div  >
                                 {red.map((r, index) => {
                                     const [role, name] = r.split('-');
-                                    return (<div style={{ backgroundImage: game.winning_side === 'red' ? `url(${victoryRedIcon})` : '', backgroundPosition: `-55px -${((index + 1) * 75) - 75}px`, marginBottom: '5px', backgroundRepeat: 'no-repeat', backgroundSize: '400px' }}>
+                                    return (<div style={{ backgroundImage: game.winning_side === 'red' ? `url(${victoryRedIcon})` : '', backgroundPosition: `-55px -${((index + 1) * 75) - 75}px`, marginBottom: '5px', backgroundRepeat: 'no-repeat', backgroundSize: '400px', borderRadius: '5px', }}>
                                         <Player name={name} role={role} color="red" winner={game.winning_side === "red"} />
                                     </div>)
                                 })}
